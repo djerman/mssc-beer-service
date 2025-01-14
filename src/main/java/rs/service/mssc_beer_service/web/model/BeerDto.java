d58b7d8636e3ae5cfaa2a23a56de3c79edbbf350
goran <goran.djermanovic@neomatic.io>
2025-01-14 19:17:34 +0100

    adding web model classes

---
+++ src/main/java/rs/service/mssc_beer_service/web/model/BeerDto.java
@@ -0,0 +1,32 @@
+package rs.service.mssc_beer_service.web.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+
+    private UUID id;
+    private Integer version;
+    
+    private OffsetDateTime createDate;
+    private OffsetDateTime lastModifiedDate;
+    
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    
+    private Long upc;
+    
+    private BigDecimal price;
+    
+    private Integer quantityOnHand;
+}
---
+++ src/main/java/rs/service/mssc_beer_service/web/model/BeerStyleEnum.java
@@ -0,0 +1,6 @@
+package rs.service.mssc_beer_service.web.model;
+
+public enum BeerStyleEnum {
+
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
