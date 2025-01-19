9ab2cba009291055f3a22810c2d3b309ae1b9e77
goran <goran.djermanovic@neomatic.io>
2025-01-19 21:29:34 +0100

    fix

--- src/test/java/rs/service/mssc_beer_service/web/controller/BeerControllerTest.java
+++ src/test/java/rs/service/mssc_beer_service/web/controller/BeerControllerTest.java
@@ -1,10 +1,7 @@
 package rs.service.mssc_beer_service.web.controller;
 
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import java.util.UUID;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
