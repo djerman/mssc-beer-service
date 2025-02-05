6c1ca921aed47f895db970d36cd009db3cf5987d
goran <goran.djermanovic@neomatic.io>
2025-02-05 19:02:46 +0100

    adding validation annotations to dto

--- src/main/java/rs/service/mssc_beer_service/web/model/BeerDto.java
+++ src/main/java/rs/service/mssc_beer_service/web/model/BeerDto.java
@@ -4,6 +4,10 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,17 +19,26 @@ import lombok.NoArgsConstructor;
 @Builder
 public class BeerDto {
 
+    @Null
     private UUID id;
+    @Null
     private Integer version;
-    
+    @Null
     private OffsetDateTime createDate;
+    @Null
     private OffsetDateTime lastModifiedDate;
     
+    @NotBlank
     private String beerName;
+    @NotNull
     private BeerStyleEnum beerStyle;
     
+    @Positive
+    @NotNull
     private Long upc;
     
+    @Positive
+    @NotNull
     private BigDecimal price;
     
     private Integer quantityOnHand;
